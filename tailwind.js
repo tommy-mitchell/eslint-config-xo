d9ecab27c4a1d184be8dddfde9b6dbc668f16932
tommy-mitchell <tmitchell7@uh.edu>
2025-01-28 19:16:22 -0600

    update tailwind

--- tailwind.js
+++ tailwind.js
@@ -1,11 +1,11 @@
 "use strict";
 
 module.exports = {
-	extends: ["plugin:tailwindcss/recommended"],
+	// TODO: does not support v4
+	// extends: ["plugin:tailwindcss/recommended"],
 	plugins: ["readable-tailwind"],
 	rules: {
 		"readable-tailwind/multiline": ["warn", {
-			group: "newLine",
 			indent: "tab",
 			preferSingleLine: true,
 			printWidth: 120,
@@ -13,9 +13,9 @@ module.exports = {
 		"readable-tailwind/no-duplicate-classes": "warn",
 		"readable-tailwind/no-unnecessary-whitespace": "warn",
 		"readable-tailwind/sort-classes": "warn",
-		"tailwindcss/classnames-order": "off",
-		"tailwindcss/no-custom-classname": ["warn", {
-			callees: ["classnames", "clsx", "ctl", "cva", "tv", "tva", "tw", "twMerge", "cn", "cnx"],
-		}],
+		// "tailwindcss/classnames-order": "off",
+		// "tailwindcss/no-custom-classname": ["warn", {
+		// 	callees: ["classnames", "clsx", "ctl", "cva", "tv", "tva", "tw", "twMerge", "cn", "cnx"],
+		// }],
 	},
 };
