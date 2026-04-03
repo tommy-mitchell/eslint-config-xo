3db86d925542b2bf78d7ba1b7f4b4174ba4159cf
tommy-mitchell <tmitchell7@uh.edu>
2026-04-03 14:24:12 -0500

    fix: actually export tanstack config

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ export { default as next } from "./next.js";
 export { default as react } from "./react.js";
 // TODO: only load if tailwind is installed
 export { default as tailwind } from "./tailwind.js";
+export { default as tanstack } from "./tanstack.js";
 export { default as xo } from "./xo.js";
